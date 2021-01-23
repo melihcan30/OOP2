b270ecb97d42812eb626ab45e67166158a50258c
melihcan serbest <mcan-serbest@hotmail.com>
2021-01-23 23:59:56 +0300

    OOP

---
+++ OOP2/CustomerManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    //İş Sınıfları // business classes
+
+    class CustomerManager
+    {
+        public void Add(Musteri musteri)
+        {
+            
+        }
+    }
+}
---
+++ OOP3/DataBaseLoggerService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class DataBaseLoggerService : ILoggerService
+    {
+        public void Log()
+        {
+            Console.WriteLine("Veritabanına loglandı");
+        }
+    }
+
+    class FileLoggerService : ILoggerService
+    {
+        public void Log()
+        {
+            Console.WriteLine("Dosyaya loglandı");
+        }
+    }
+}
